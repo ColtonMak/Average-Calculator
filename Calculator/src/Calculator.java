e187a9ffcf477e672fb8fc8827a65fe62f1bd60e
Colton Mak <colton.mak1009@gmail.com>
2018-04-07 23:59:04 -0400

    4-Function Calculator

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,27 +1,70 @@
-import java.util.Scanner;
+import java.util.*;
+import org.apache.commons.lang3.ArrayUtils;
 
 public class Calculator {
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int intOne = sc.nextInt();
-		char operator = sc.next().charAt(0);
-		int intTwo = sc.nextInt();
+		String input = sc.nextLine();
 		sc.close();
-				
-		if (operator == '+') {
-			System.out.println(intOne + intTwo);
+		
+		int operatorCount = charCounter(input, "+") + charCounter(input, "-") + charCounter(input, "*") + charCounter(input, "/");
+		int j=0;
+		input = input + "+";
+		
+		String[] inputArr = new String[operatorCount*2+1];
+		for (int i=0; i<inputArr.length; i++) {
+			String temp = Character.toString(input.charAt(j));
+			j++;
+			while (input.charAt(j-1) != '+' && input.charAt(j-1) != '-' && input.charAt(j-1) != '*' && input.charAt(j-1) != '/' && input.charAt(j) != '+' && input.charAt(j) != '-' && input.charAt(j) != '*' && input.charAt(j) != '/') {
+				temp = temp + Character.toString(input.charAt(j));
+				j++;
+			}
+			inputArr[i] = temp;
 		}
-		else if (operator == '-') {
-			System.out.println(intOne - intTwo);
 
-		}
-		else if (operator == '*') {
-			System.out.println(intOne * intTwo);
+		inputArr = operator(inputArr, "*");
+		inputArr = operator(inputArr, "/");
+		inputArr = operator(inputArr, "+");
+		inputArr = operator(inputArr, "-");
 
+		System.out.println(inputArr[0]);
+	}
+	
+	public static void arrTester(String[] arr, String arrName) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arrName + " array, index " + i + ": " + arr[i]);
 		}
-		else {
-			System.out.println(intOne / intTwo);
-
+	}
+	
+	public static int charCounter(String input, String ch) {
+		int count = input.length() - input.replace(ch, "").length();
+		return count;
+	}
+	
+	public static String[] operator(String[] arr, String operator) {
+		for (int i=0; i<arr.length; i++) {
+			if (arr[i].equals(operator)) {
+				double numOne = Double.parseDouble(arr[i-1]);
+				double numTwo = Double.parseDouble(arr[i+1]);
+				switch (operator) {
+				case "*":
+					arr[i-1] = String.valueOf(numOne * numTwo);
+					break;
+				case "/":
+					arr[i-1] = String.valueOf(numOne / numTwo);
+					break;
+				case "+":
+					arr[i-1] = String.valueOf(numOne + numTwo);
+					break;
+				case "-":
+					arr[i-1] = String.valueOf(numOne - numTwo);
+					break;
+				}
+				arr = ArrayUtils.remove(arr, i);
+				arr = ArrayUtils.remove(arr, i);
+			}
 		}
+		return arr;
 	}
 }
